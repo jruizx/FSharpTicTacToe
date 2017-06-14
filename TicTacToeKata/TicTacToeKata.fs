e5456b0cadeb3f5b5e41e5bfc476139e7255e086
Jordi Ruiz <jruizx@gmail.com>
2017-06-14 19:14:05 +0100

    14/06/2017

--- TicTacToeKata/TicTacToeKata.fs
+++ TicTacToeKata/TicTacToeKata.fs
@@ -26,7 +26,7 @@ If all nine squares are filled and neither player has three in a row, the game i
         type Movement = {player: Player; location: Location}
         type Movements = Movement list
 
-        type Status = InvalidPlayer | InvalidPlayerAndMove | InvalidMove | Accepted
+        type Status = InvalidPlayer | InvalidPlayerAndMove | InvalidMove | Accepted | XWin | YWin | Draw
 
         type PlayResult = Movements * Status
 
@@ -36,15 +36,26 @@ If all nine squares are filled and neither player has three in a row, the game i
         let isPlayerSameAsLast movements movement =
             (movements |> List.last).player = movement.player
 
+        let isWinner movements =
+            if(movements |> List.filter (fun x -> x.player = X) |> List.filter (fun x -> x.location.y = One) |> List.length = 3) then
+                XWin
+            else if (movements |> List.filter (fun x -> x.player = X) |> List.filter (fun x -> x.location.y = Two) |> List.length = 3) then
+                XWin
+            else if (movements |> List.filter (fun x -> x.player = X) |> List.filter (fun x -> x.location.y = Three) |> List.length = 3) then
+                XWin
+            else
+                Accepted   
+             
+
         let play (movement:Movement) (movements:Movements) =
             match movement, movements with
             | {player = O}, [] -> (movements, InvalidPlayer)
             | {player = X}, [] -> ([movement], Accepted)
-            | m, ms -> match (isPlayerSameAsLast ms m, moveExists ms m ) with
+            | m, ms -> match (isPlayerSameAsLast ms m, moveExists ms m)  with
                        | true, true -> (movements, InvalidPlayerAndMove)
                        | true, _ -> (movements, InvalidPlayer)
                        | _, true -> (movements, InvalidMove)
-                       | _, _ -> (movements @ [movement], Accepted)
+                       | _, _ -> (movements @ [movement], isWinner (movements @ [movement]))
 
 
     module TicTacToeTests =
@@ -92,3 +103,42 @@ If all nine squares are filled and neither player has three in a row, the game i
             |> play  movement
             |> fst 
             |> play movement2 =! ([movement], InvalidMove)
+
+        [<Test>]
+        let ``X wins when he has three horizontal movements on the first row`` () =
+            let movement1 = {player = X; location = {x = One; y = One }}
+            let movement2 = {player = O; location = {x = One; y = Two }}
+            let movement3 = {player = X; location = {x = Two; y = One }}
+            let movement4 = {player = O; location = {x = Two; y = Two }}
+            let movement5 = {player = X; location = {x = Three; y = One }}
+
+            []
+            |> play  movement1
+            |> fst 
+            |> play movement2
+            |> fst 
+            |> play movement3
+            |> fst 
+            |> play movement4
+            |> fst 
+            |> play movement5 =! ([movement1; movement2; movement3; movement4; movement5], XWin)
+
+        [<Test>]
+        let ``X wins when he has three horizontal movements on the second row`` () =
+            let movement1 = {player = X; location = {x = One; y = Two }}
+            let movement2 = {player = O; location = {x = One; y = One }}
+            let movement3 = {player = X; location = {x = Two; y = Two }}
+            let movement4 = {player = O; location = {x = Two; y = One }}
+            let movement5 = {player = X; location = {x = Three; y = Two }}
+
+            []
+            |> play  movement1
+            |> fst 
+            |> play movement2
+            |> fst 
+            |> play movement3
+            |> fst 
+            |> play movement4
+            |> fst 
+            |> play movement5 =! ([movement1; movement2; movement3; movement4; movement5], XWin)
+
